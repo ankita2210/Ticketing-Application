ab3b3f70f33e1ff84e0eb4eebc87ee6edddf2279
ankita2210 <ankita.nasipuri@gmail.com>
2023-11-26 20:58:00 +0530

    order test fixes

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,69 +1,96 @@
-import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
-import { Order, OrderStatus } from '../../models/order';
 import { Ticket } from '../../models/ticket';
-import { natsWrapper } from '../../__mocks__/nats-wrapper';
+import { natsWrapper } from '../../nats-wrapper';
 
-it('returns an error if the ticket does not exist', async () => {
-  const ticketId = new mongoose.Types.ObjectId();
+it('has a route handler listening to /api/tickets for post requests', async () => {
+  const response = await request(app).post('/api/tickets').send({});
 
-  await request(app)
-    .post('/api/orders')
-    .set('Cookie', global.signin())
-    .send({ ticketId })
-    .expect(404);
+  expect(response.status).not.toEqual(404);
 });
 
-it('returns an error if the ticket is already reserved', async () => {
-  const ticket = Ticket.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    title: 'concert',
-    price: 20,
-  });
-  await ticket.save();
-  const order = Order.build({
-    ticket,
-    userId: 'laskdflkajsdf',
-    status: OrderStatus.Created,
-    expiresAt: new Date(),
-  });
-  await order.save();
+it('can only be accessed if the user is signed in', async () => {
+  await request(app).post('/api/tickets').send({}).expect(401);
+});
+
+it('returns a status other than 401 if the user is signed in', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({});
+
+  expect(response.status).not.toEqual(401);
+});
+
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
 
   await request(app)
-    .post('/api/orders')
+    .post('/api/tickets')
     .set('Cookie', global.signin())
-    .send({ ticketId: ticket.id })
+    .send({
+      price: 10,
+    })
     .expect(400);
 });
 
-it('reserves a ticket', async () => {
-  const ticket = Ticket.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    title: 'concert',
-    price: 20,
-  });
-  await ticket.save();
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asldkjf',
+      price: -10,
+    })
+    .expect(400);
 
   await request(app)
-    .post('/api/orders')
+    .post('/api/tickets')
     .set('Cookie', global.signin())
-    .send({ ticketId: ticket.id })
-    .expect(201);
+    .send({
+      title: 'laskdfj',
+    })
+    .expect(400);
 });
 
-it('emits an order created event', async () => {
-  const ticket = Ticket.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    title: 'concert',
-    price: 20,
-  });
-  await ticket.save();
+it('creates a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const title = 'asldkfj';
 
   await request(app)
-    .post('/api/orders')
+    .post('/api/tickets')
     .set('Cookie', global.signin())
-    .send({ ticketId: ticket.id })
+    .send({
+      title,
+      price: 20,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
+});
+
+it('publishes an event', async () => {
+  const title = 'asldkfj';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price: 20,
+    })
     .expect(201);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,56 +1,31 @@
 import request from 'supertest';
 import { app } from '../../app';
-import { Ticket } from '../../models/ticket';
 import mongoose from 'mongoose';
 
-it('fetches the order', async () => {
-  // Create a ticket
-  const ticket = Ticket.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    title: 'concert',
-    price: 20,
-  });
-  await ticket.save();
+it('returns a 404 if the ticket is not found', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
 
-  const user = global.signin();
-  // make a request to build an order with this ticket
-  const { body: order } = await request(app)
-    .post('/api/orders')
-    .set('Cookie', user)
-    .send({ ticketId: ticket.id })
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
+});
+
+it('returns the ticket if the ticket is found', async () => {
+  const title = 'concert';
+  const price = 20;
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price,
+    })
     .expect(201);
 
-  // make request to fetch the order
-  const { body: fetchedOrder } = await request(app)
-    .get(`/api/orders/${order.id}`)
-    .set('Cookie', user)
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
     .send()
     .expect(200);
 
-  expect(fetchedOrder.id).toEqual(order.id);
-});
-
-it('returns an error if one user tries to fetch another users order', async () => {
-  // Create a ticket
-  const ticket = Ticket.build({
-    id: new mongoose.Types.ObjectId().toHexString(),
-    title: 'concert',
-    price: 20,
-  });
-  await ticket.save();
-
-  const user = global.signin();
-  // make a request to build an order with this ticket
-  const { body: order } = await request(app)
-    .post('/api/orders')
-    .set('Cookie', user)
-    .send({ ticketId: ticket.id })
-    .expect(201);
-
-  // make request to fetch the order
-  await request(app)
-    .get(`/api/orders/${order.id}`)
-    .set('Cookie', global.signin())
-    .send()
-    .expect(401);
+  expect(ticketResponse.body.title).toEqual(title);
+  expect(ticketResponse.body.price).toEqual(price);
 });
